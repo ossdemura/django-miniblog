8338fa27a9f4e2437833efddc80085dc485ab84b
juannsj <juannsj@gmail.com>
2017-11-30 17:33:30 +0100

    Modified even representation in the calendar day cell

--- src/events/models.py
+++ src/events/models.py
@@ -19,4 +19,4 @@ class Event(models.Model):
 
     def get_absolute_url(self):
         url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
-        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
+        return u'<p><a href="%s">%s</a></p>' % (url, str(self.start_time) + " : " + str(self.end_time) + " - " + str(self.notes)  )
